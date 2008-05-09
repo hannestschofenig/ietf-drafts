ba7a339294c91bf2fc4a467bf69b49d7314f61d3
ekr <ekr@c6c94066-4ede-0310-846a-e20a13935aba>
2008-05-09 19:25:38 +0000

    Update
    git-svn-id: https://svn.resiprocate.org/rep/ietf-drafts/ekr@2875 c6c94066-4ede-0310-846a-e20a13935aba

---
+++ dtls/fix.pl
@@ -0,0 +1,12 @@
+#!/usr//bin/perl
+$* = 1;                                 # Enable multiline patterns.
+undef $/;                               # Read whole files in a single
+                                        # gulp.
+
+while (<>) {                            # Read the entire input file.
+    s/FORMFEED(\[Page\s+\d+\])\s+/        \1\n\f\n/g;
+                                        # Rewrite the end-of-pages.
+#    s/\f\n$/\f/;                       # Want bare formfeed at end?
+#    s/\f\n$//;                         # Want no formfeed at end?
+    print;                              # Print the resultant file.
+}
---
+++ dtls/mktoc.pl
@@ -0,0 +1,27 @@
+#!/usr/local/bin/perl
+$page=1;
+
+while(<STDIN>){
+  $page++ if(/\[Page \d+\]/);
+  
+  if(/^(Appendix\s+)?([\dA-F]+\.\S*)\s+(.*)/){
+    chop;
+
+    $num=$2;
+    $entry=$3;
+
+    $pad=10-length($num);
+    $line=$num;
+    for($i=0;$i<$pad;$i++){
+      $line.=" ";
+    }
+    $line.=$entry;
+    $pad=67-length($line);
+    for($i=0;$i<$pad;$i++){
+      $line.=" ";
+    }
+    $line.=$page;
+    print "$line\n";
+    
+  }
+}
